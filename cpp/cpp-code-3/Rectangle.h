8f9ea1083025eeedfd78747b9903025ab8a83502
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-15 02:06:21 +0100

    Add XML output for class inheritance

---
+++ cpp/cpp-code-3/Rectangle.h
@@ -0,0 +1,22 @@
+#include "Polygon.h"
+
+/// This class represents a rectangle.
+class Rectangle : public Polygon
+{
+public:
+    void draw() override;
+
+    /// Gets the width of the rectangle.
+    /// @return The width of the rectangle.
+    int width() const;
+    /// Gets the height of the rectangle.
+    /// @return The height of the rectangle.
+    int height() const;
+
+    /// Sets the width of the rectangle.
+    /// @param w The new width.
+    void setWidth(int w);
+    /// Sets the height of the rectangle.
+    /// @param h The new height.
+    void setHeight(int h);
+};
