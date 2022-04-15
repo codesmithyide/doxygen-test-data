92d33c1727c0ee8be360af99a7914831f6bb487d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-16 00:56:30 +0100

    Add XML output for a group

---
+++ cpp/cpp-group-1/Polygon.h
@@ -0,0 +1,13 @@
+/**
+\defgroup Shapes Shapes: classes that represent geometric shapes.
+*/
+
+
+/// This class represents a polygon.
+/// \ingroup Shapes
+class Polygon
+{
+public:
+    /// Draw the polygon
+    virtual void draw() = 0;
+};
